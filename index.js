b19c713e7ea10ba7445b5269a0edad85f8b81feb
Matt Travi <programmer@travi.org>
2015-04-25 21:04:57 -0500

    configured karma to load mocha-given to the browser

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+'use strict';
+
+var path = require('path');
+
+function framework(files) {
+    files.unshift({
+        pattern: path.dirname(require.resolve('mocha-given')) + '/index.js',
+        included: true,
+        served: true,
+        watched: false
+    });
+}
+
+framework.$inject = ['config.files'];
+
 module.exports = {
-    'framework:mocha-given': ['framework']
+    'framework:mocha-given': ['framework', framework]
 };
--- test/adapterTest.js
+++ test/adapterTest.js
@@ -1,10 +1,31 @@
-var adapter = require('../index'),
-    assert = require('chai').assert;
+var adapter = require('../index')['framework:mocha-given'],
+    assert = require('chai').assert,
+    path = require('path');
 
 suite('Mocha-Given Karma adapter tests', function () {
     'use strict';
 
     test('that the karma dependency injection format is met', function () {
-        assert.equal(adapter['framework:mocha-given'][0], 'framework');
+        assert.equal(adapter[0], 'framework');
+        assert.isFunction(adapter[1]);
+    });
+
+    test('that the file list will be injected', function () {
+        assert.equal(adapter[1].$inject[0], 'config.files');
+    });
+
+    test('that the mocha-given adapter is added to the file list', function () {
+        var fileList = [],
+            originalFileCount = fileList.length;
+
+        adapter[1](fileList);
+
+        assert.equal(fileList.length, originalFileCount + 1);
+        assert.include(fileList, {
+            pattern: path.dirname(require.resolve('mocha-given') + '/index.js'),
+            included: true,
+            served: true,
+            watched: false
+        });
     });
 });
